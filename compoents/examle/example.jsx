eac1415d15e3e8a429be4a6a4293169bbd5d9d98
Redither <slavonchik68@gmail.com>
2023-03-06 05:21:50 +0300

    created eample component

---
+++ compoents/examle/example.jsx
@@ -0,0 +1,15 @@
+import { Container } from '@mui/system';
+import React from 'react';
+
+const Example = () => {
+    return (
+        <Container>
+            <h1>Это пример компонента</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem adipisci atque, accusamus voluptate similique facilis nisi reiciendis eum unde dolores! At magni maxime necessitatibus rerum expedita debitis id pariatur eius.
+            </p>
+        </Container>
+    );
+};
+
+export default Example;
