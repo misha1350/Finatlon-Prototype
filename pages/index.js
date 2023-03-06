57b7976983c3c8b4419fc38451e3ccab57969d25
Redither <slavonchik68@gmail.com>
2023-03-06 05:23:17 +0300

    added showing example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Example from '../compoents/examle/example';
+import { PageTemplate } from '../widgets/pageTemplate/PageTemplate';
 
 const index = () => {
     return (
-        <>
-        </>
+        <PageTemplate>
+            <Example/>
+        </PageTemplate>
     );
 };
 
